f1be9bde9d1ea6f54c03657887d8812f5f98f695
Manas <manas18244@iiitd.ac.in>
2019-11-01 00:33:58 +0530

    header file included

---
+++ maysh.h
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <errno.h>
+#include <dirent.h>
+
+#define BUFFER_SIZE     16	// space for storing each space-separated commands
+#define SPACE_SEP_WORDS 8	// space for space-separated words
+
+int inputStream(char *, int *);
+int parseStream(char *[], char *, int);
+int checkExit(char *);
+void strncopy(char [], char [], int);
+void strcopy(char [], char []);
+void executeCMD_extern(char *[], int);
+void executeCMD_intern(char *[], int);
+void executeCMD_cd(char *[], int);
+void executeCMD_history(char *[], int);
+void executeCMD_pwd(char *[], int);
+void executeHelp(char *[], int);
+
